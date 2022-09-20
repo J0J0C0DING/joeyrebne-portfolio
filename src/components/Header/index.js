fee99f4c8dfd9125c7c4a6d309c52880eda25dd5
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-09-19 22:27:40 -0500

    About section at MVP

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header className="w-screen flex flex-wrap justify-center pt-5 items-center sticky top-0 pb-3 bg-white">
-      <div className="mb-4">
+      <div className="mb-2">
         <img
           src={require('../../assets/images/branding/logo-full/full-logo-black.png')}
           alt="logo"
